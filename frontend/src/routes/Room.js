7b5de7da3600182189ae9af880c89570a767f4a5
Md. Sharif Alam <priom7197@gmail.com>
2020-10-19 22:29:24 +0600

    create serve config with socket io

--- frontend/src/routes/Room.js
+++ frontend/src/routes/Room.js
@@ -52,6 +52,26 @@ const Room = props => {
       );
   }
 
+  function createPeer(userID) {
+    const peer = new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: "stun:stun.stunprotocol.org"
+        },
+        {
+          urls: "turn:numb.viagenie.ca",
+          credential: "muazkh",
+          username: "webrtc@live.com"
+        }
+      ]
+    });
+    peer.onicecandicate = handleIceCandicate;
+    peer.ontrack = handleTrackEvent;
+    peer.onnegotiationneeded = () =>
+      handleNegotiationNeededEvent(userID);
+    return peer;
+  }
+
   return (
     <div>
       <video autoPlay ref={myVideo}></video>
